44b59e815ed118df38fd5d68ba9f983d10353e08
Víctor Gallardo Luque <vbubobuho@gmail.com>
2019-09-25 18:05:25 +0200

    comments added to the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+/**
+ * This is Java code.
+ * File of this code should be save with the name HelloWorld.java.
+ *
+ * @author Víctor Gallardo.
+ */
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("¡Hello world!");
